4f6f439615354ab81c01097c38bee2f539db6d51
Andrew Smith <848smith@gmail.com>
2020-03-01 05:30:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs
   
-  extend Findable::ClassMethods
+  extend Findable
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable
